422a887c3f02ce9b149deb1a3ee430f9800c22be
torburg <torburg@yandex.ru>
2020-03-29 21:21:13 +0300

    Created EditView

--- avswTest/Controller/EditViewController.swift
+++ avswTest/Controller/EditViewController.swift
@@ -10,7 +10,44 @@ import UIKit
 
 class EditViewController: UIViewController {
 
+    @IBOutlet weak var navigationBar: UINavigationBar!
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var attributes: UITableView!
+    @IBAction func addNewAttribute(_ sender: Any) {
+    }
+    @IBAction func deletePerson(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        setView()
+    }
+
+    func setView() {
+        let navigationItem = UINavigationItem()
+        let discardChangesButton = UIBarButtonItem(title: "Discard Changes", style: .plain, target: self, action: #selector(discardChanges))
+        discardChangesButton.tintColor = .systemRed
+        navigationItem.leftBarButtonItem = discardChangesButton
+        let saveChangesButton = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(saveChanges))
+        saveChangesButton.tintColor = .systemGreen
+        navigationItem.rightBarButtonItem = saveChangesButton
+        navigationBar.items = [navigationItem]
+    }
+
+    @objc func discardChanges() {
+        print(#function)
+    }
+    @objc func saveChanges() {
+        print(#function)
+    }
+}
+
+extension EditViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
     }
 }
